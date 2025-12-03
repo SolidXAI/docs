a8375df7a2fae490ddd0c8118011c640d4402abe
Saibaz.Khan <saibaz.khan@logicloop.io>
2025-12-03 13:22:07 +0530

    script-env-path-change

--- typesense_Server/run-typesense.sh
+++ typesense_Server/run-typesense.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Load parent directory .env
-export $(grep -v '^#' ../.env | xargs)
+export $(grep -v '^#' .env | xargs)
 
 # Run Typesense using env variables
 ./typesense-server \
-  --data-dir $TYPESENSE_DATA_DIR \
-  --api-key $TYPESENSE_API_KEY \
-  --listen-address $TYPESENSE_HOST \
-  --listen-port $TYPESENSE_PORT \
+  --data-dir "$TYPESENSE_DATA_DIR" \
+  --api-key "$TYPESENSE_API_KEY" \
+  --listen-address "$TYPESENSE_HOST" \
+  --listen-port "$TYPESENSE_PORT" \
   --enable-cors
