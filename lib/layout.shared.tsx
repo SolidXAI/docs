12e3544bcd0e8a09fa70f8ad7705e4b1079d68a2
Pathik <pbotadra@gmail.com>
2026-05-26 23:41:16 +0530

    fix for menu duplication

--- lib/layout.shared.tsx
+++ lib/layout.shared.tsx
@@ -7,12 +7,6 @@ export function baseOptions(): BaseLayoutProps {
         <img src="/img/NavbarLogo.svg" alt="SolidX" className="h-6" />
       ),
     },
-    links: [
-      { text: 'Quick Start', url: '/docs/quick-start' },
-      { text: 'Tutorial', url: '/docs/tutorial', active: 'nested-url' },
-      { text: 'Admin', url: '/docs/admin-docs', active: 'nested-url' },
-      { text: 'Reference', url: '/docs/developer-docs', active: 'nested-url' },
-      { text: 'Recipes', url: '/docs/recipes', active: 'nested-url' },
-    ],
+    links: [],
   };
 }
